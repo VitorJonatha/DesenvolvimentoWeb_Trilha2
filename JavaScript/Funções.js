8ed6a58a7f6baea49dc4ecb4be1fabc54e90ab1f
VitorJonatha <vitoramorim2918@gmail.com>
2022-11-30 16:00:11 -0300

    30-11-22

---
+++ JavaScript/Funções.js
@@ -0,0 +1,72 @@
+//função para somar dois números
+function calcular(n1,n2)
+{
+    console.log(n1+n2)
+}
+calcular(5,5)
+
+//Divisão de dois números
+function dividir(n1,n2)
+{
+    console.log(n1/n2)
+}
+dividir(20,2)
+
+//Área de um triãngulo
+function areatri(b,a)
+{
+    console.log((b*a)/2)
+}
+areatri(20,10)
+
+//ERRO caso -18
+function id (idade){
+
+if (idade < 18)
+    {
+    console.log('ERRO')
+    }
+else
+    {
+    console.log('OK')
+    }
+}
+id (18)
+
+//altura em centímetros
+function alt(a)
+{
+    console.log(a*100,'centímetros')
+}
+alt(1.80)
+
+//anos,meses,dias
+function ida(i)
+{
+    console.log(i,"anos")
+    console.log(i*12,"meses")
+    console.log(i*365,"dias")
+}
+ida(18)
+
+//madia e a soma de 5 notas
+function media(m1,m2,m3,m4,m5)
+{
+    console.log(m1+m2+m3+m4+m5)
+    console.log((m1+m2+m3+m4+m5)/5)
+}
+media(2,2,2,2,2)
+
+//área de um círculo
+function raio(r)
+{
+    console.log(3.14*(r*r))
+}
+raio(3,4)
+
+//idade em 2025
+function data(d)
+{
+    console.log(2025-d)
+}
+data(2004)
